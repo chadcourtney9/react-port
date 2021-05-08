935f3c40e8155f73e66033adc536b9e5eef79b8e
Chad Courtney <Chadcourtney567@gmail.com>
2021-05-08 12:58:41 -0400

    added react router

--- src/components/mainContainer.js
+++ src/components/mainContainer.js
@@ -1,23 +1,35 @@
 import React from "react";
 import PortNav from "./navBar/navBar.js"
-import { Container } from "react-bootstrap"
 import { Component } from "react";
-import AboutMe from "./AboutMe";
+import AboutMe from "./aboutme/AboutMe";
 import Portfolio from "./portfolio/Portfolio.js";
+import Footer from "./footer/footer.js";
+import "./portfolio/Portcss.css";
+import Intro from "./intro/intro";
+import "./maincss.css";
+import { Switch, Route } from "react-router-dom";
+import { ABOUT_PATH, PORTFOLIO_PATH, INTRO_PATH } from "../viewRoutes";
+
 
 class MainContainer extends Component {
 
-
     render() {
         return (
-            <div>
+            <div className="body-css">
                 <PortNav />
-                <Container>
-                    <AboutMe />
-                    <Portfolio> </Portfolio>
-                </Container>
+                <Switch>
+                    <Route exact={true} path={INTRO_PATH}>
+                        <Intro />
+                    </Route>
+                    <Route exact={true} path={ABOUT_PATH}>
+                        <AboutMe />
+                    </Route>
+                    <Route exact={true} path={PORTFOLIO_PATH}>
+                        <Portfolio />
+                    </Route>
+                </Switch>
+                <Footer />
             </div>
-
         )
     }
 }
