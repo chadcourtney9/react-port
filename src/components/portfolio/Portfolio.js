d85f8de012f5348c6645d339458a2ba3b740a4b9
Chad Courtney <Chadcourtney567@gmail.com>
2021-05-08 14:23:48 -0400

    chagned the links within the images

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -7,6 +7,7 @@ import BudgetTracker from "../images/budget-tracker.PNG";
 import FitnessTracker from "../images/fitnessTracker.PNG";
 import "./Portcss.css";
 import { Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function Portfolio() {
     return (
@@ -21,7 +22,10 @@ function Portfolio() {
                             <div className="content-details fadeIn-bottom">
                                 <h3 className="content-title">Character Creator Express</h3>
                                 <p className="content-text">Quickly create Character for D&D campaings. Click background to see page!</p>
-                                <a className="content-text" href="https://github.com/chadcourtney9/dnd-quick-character-creator">Code</a>
+                                <Link style={{ textDecoration: "none", color: "grey" }}
+                                    className="content-text"
+                                    href="https://github.com/chadcourtney9/dnd-quick-character-creator"><i
+                                        className="fab fa-github"></i>Code</Link>
                             </div>
                         </a>
                     </div>
@@ -34,7 +38,10 @@ function Portfolio() {
                             <div className="content-details fadeIn-bottom">
                                 <h3 className="content-title">Suprise Me-al</h3>
                                 <p className="content-text">Quickly find meals around the world to make for dinner! Click background to see page!</p>
-                                <a className="content-text" href="https://github.com/chadcourtney9/dnd-quick-character-creator">Code</a>
+                                <Link style={{ textDecoration: "none", color: "grey" }}
+                                    className="content-text"
+                                    href="https://github.com/chadcourtney9/dnd-quick-character-creator"><i
+                                        className="fab fa-github"></i>Code</Link>
                             </div>
                         </a>
                     </div>
@@ -45,9 +52,13 @@ function Portfolio() {
                             <div className="content-overlay"></div>
                             <img className="content-image" src={FitnessTracker} alt="Dnd page" />
                             <div className="content-details fadeIn-bottom">
-                                <h3 className="content-title">Suprise Me-al</h3>
-                                <p className="content-text">Click to see page!</p>
-                                <a className="content-text" href="https://github.com/chadcourtney9/Fitness-Tracker">Code</a>
+                                <h3 className="content-title">Fitness Tracker</h3>
+                                <p className="content-text">An app to help keep track of your workouts throughout the week. Click background to see page!</p>
+                                <Link
+                                    style={{ textDecoration: "none", color: "grey" }}
+                                    className="content-text"
+                                    href="https://github.com/chadcourtney9/Fitness-Tracker"><i
+                                        className="fab fa-github"></i>Code</Link>
                             </div>
                         </a>
                     </div>
@@ -60,7 +71,11 @@ function Portfolio() {
                             <div className="content-details fadeIn-bottom">
                                 <h3 className="content-title">Random Password Generator</h3>
                                 <p className="content-text">Generate a strong random password with selected characters! Click background to see page!</p>
-                                <a className="content-text" href="https://github.com/chadcourtney9/Random-Password-Gen">Code</a>
+                                <Link
+                                    style={{ textDecoration: "none", color: "grey" }}
+                                    className="content-text"
+                                    href="https://github.com/chadcourtney9/Random-Password-Gen"><i
+                                        className="fab fa-github"></i>Code</Link>
                             </div>
                         </a>
                     </div>
@@ -71,9 +86,13 @@ function Portfolio() {
                             <div className="content-overlay"></div>
                             <img className="content-image" src={BudgetTracker} alt="Dnd page" />
                             <div className="content-details fadeIn-bottom">
-                                <h3 className="content-title">Suprise Me-al</h3>
-                                <p className="content-text">Click to see page!</p>
-                                <a className="content-text" href="https://github.com/chadcourtney9/budget-tracker">Code</a>
+                                <h3 className="content-title">Budget Tracker</h3>
+                                <p className="content-text">This app helps you manage your spending online or offline! Click to see page!</p>
+                                <Link
+                                    style={{ textDecoration: "none", color: "grey" }}
+                                    className="content-text"
+                                    href="https://github.com/chadcourtney9/budget-tracker"><i
+                                        className="fab fa-github"></i>Code</Link>
                             </div>
                         </a>
                     </div>
@@ -86,7 +105,10 @@ function Portfolio() {
                             <div className="content-details fadeIn-bottom">
                                 <h3 className="content-title">Daily Planner</h3>
                                 <p className="content-text">Sort daily tasks based hourly to organize your day! Click background to see page!</p>
-                                <a className="content-text" href="https://github.com/chadcourtney9/Daily-Planner">Code</a>
+                                <Link
+                                    style={{ textDecoration: "none", color: "grey" }}
+                                    className="content-text"
+                                    href="https://github.com/chadcourtney9/Daily-Planner"><i className="fab fa-github"></i>Code</Link>
                             </div>
                         </a>
                     </div>
