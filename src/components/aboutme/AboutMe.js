bb8a2d37a6d12060b83cc738f870be027426e404
Chad Courtney <Chadcourtney567@gmail.com>
2021-05-09 17:26:21 -0400

    readme/aboutme changes

--- src/components/aboutme/AboutMe.js
+++ src/components/aboutme/AboutMe.js
@@ -4,7 +4,6 @@ import ChadCap from ".././images/Capture2.PNG"
 import "./aboutmecss.css";
 import { Jumbotron, Container } from "react-bootstrap";
 
-
 class AboutMe extends Component {
 
     render() {
@@ -17,6 +16,9 @@ class AboutMe extends Component {
                         <p className="amP">
                             Hello, my name is Chad Courtney. I have recently started taking up classes to start learning coding. I am currently enrolled at UNH's bootcamp, I have been building computers for about 7 years and learning a lot about the hardward that goes into them. I'm an avid lover of eSports and my goal is to work for a professional gaming team. I have followed the League of Legends Champion Series for the last 5-6 years and would love to work in the U.S. If I had to make a choice for a team to work for it would definitely be Team Liquid. They have a great brand and from what I can see an amazing enviorment with incredibly great leaders. By 2022 I hope to be a developer for them in either the States or their facility in the Netherlands!
                         </p>
+                        <br></br>
+                        <li><a style={{ textDecoration: "none", color: "grey" }} className="cool-link" href="tel: 9784573908" >Phone</a></li>
+                        <li><a style={{ textDecoration: "none", color: "grey" }} className="cool-link" href="mailto: chadcourtney567@gmail.com" >Email</a></li>
                     </Container>
                 </Jumbotron>
             </div >
