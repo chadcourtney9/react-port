5f399a367c03f0f2773c15d56a1f42fcf9b4f35c
Chad Courtney <Chadcourtney567@gmail.com>
2021-06-30 16:06:26 -0400

    updated my about me and portfolio

--- src/components/aboutme/AboutMe.js
+++ src/components/aboutme/AboutMe.js
@@ -14,7 +14,7 @@ class AboutMe extends Component {
                         <h1 className="amHeader">About Me</h1>
                         <img src={ChadCap} alt="lumberChad" className="lumberChad" />
                         <p className="amP">
-                            Hello, my name is Chad Courtney. I have recently started taking up classes to start learning coding. I am currently enrolled at UNH's bootcamp, I have been building computers for about 7 years and learning a lot about the hardward that goes into them. I'm an avid lover of eSports and my goal is to work for a professional gaming team. I have followed the League of Legends Champion Series for the last 5-6 years and would love to work in the U.S. If I had to make a choice for a team to work for it would definitely be Team Liquid. They have a great brand and from what I can see an amazing enviorment with incredibly great leaders. By 2022 I hope to be a developer for them in either the States or their facility in the Netherlands!
+                            Hello, my name is Chad Courtney. I am a Full Stack Developer with a real strong eye for aesthetics. Also have strong teamwork and coordination skills after working with agile development practices creating full-stack web apps.  Always seeking to improve my skills, with both frontend and backend languages! If you're interested in working together, feel free to reach out to me through the any of the links below! Graduated from University of New Hampshire in Full Stack Web Development.
                         </p>
                         <br></br>
                         <li><a style={{ textDecoration: "none", color: "grey" }} className="cool-link" href="tel: 9784573908" >Phone</a></li>
--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,7 +1,7 @@
 import React from "react";
 import DndSlide1 from "../images/DndHome.png";
 import Meal from "../images/mealFix.PNG";
-import Planner from "../images/dailyplannerfix.PNG";
+import DevSets from "../images/devsetsResize.PNG";
 import RGen from "../images/randompassword gen.PNG";
 import BudgetTracker from "../images/budget-tracker.PNG";
 import FitnessTracker from "../images/fitnessTracker.PNG";
@@ -16,14 +16,32 @@ function Portfolio() {
             <Row className="portRow">
                 <Container className="portContainer col-md-6">
                     <div className="content">
-                        <a href="https://character-creator-express.herokuapp.com/login" rel="noreferrer" target="_blank">
+                        <a href="https://devsets-d95a1.web.app/" rel="noreferrer" target="_blank">
+                            <div className="content-overlay"></div>
+                            <img className="content-image" src={DevSets} alt="Dev Sets" />
+                            <div className="content-details fadeIn-bottom">
+                                <h3 className="content-title">Dev Sets</h3>
+                                <p className="content-text">Allows remote workers to share their set ups or get ideas from other users!</p>
+                                <Link
+                                    style={{ textDecoration: "none", color: "grey" }}
+                                    className="content-code-link"
+                                    href="https://github.com/chadcourtney9/dev-sets"><i
+                                        className="fab fa-github"></i> Code</Link>
+                            </div>
+                        </a>
+                    </div>
+
+                </Container>
+                <Container className="portContainer col-md-6">
+                    <div className="content">
+                        <a href="http://dnd-exp.herokuapp.com/login" rel="noreferrer" target="_blank">
                             <div className="content-overlay"></div>
                             <img className="content-image" src={DndSlide1} alt="Dnd page" />
                             <div className="content-details fadeIn-bottom">
                                 <h3 className="content-title">Character Creator Express</h3>
                                 <p className="content-text">Quickly create Character for D&D campaings. Click background to see page!</p>
                                 <Link style={{ textDecoration: "none", color: "grey" }}
-                                    className="content-text"
+                                    className="content-code-link"
                                     href="https://github.com/chadcourtney9/dnd-quick-character-creator"><i
                                         className="fab fa-github"></i> Code</Link>
                             </div>
@@ -32,15 +50,15 @@ function Portfolio() {
                 </Container>
                 <Container className="portContainer col-md-6">
                     <div className="content">
-                        <a href="https://character-creator-express.herokuapp.com/login" rel="noreferrer" target="_blank">
+                        <a href="https://tjvig94.github.io/random-meal-generator/" rel="noreferrer" target="_blank">
                             <div className="content-overlay"></div>
-                            <img className="content-image" src={Meal} alt="Dnd page" />
+                            <img className="content-image" src={Meal} alt="Suprise Me-al" />
                             <div className="content-details fadeIn-bottom">
                                 <h3 className="content-title">Suprise Me-al</h3>
                                 <p className="content-text">Quickly find meals around the world to make for dinner! Click background to see page!</p>
                                 <Link style={{ textDecoration: "none", color: "grey" }}
-                                    className="content-text"
-                                    href="https://github.com/chadcourtney9/dnd-quick-character-creator"><i
+                                    className="content-code-link"
+                                    href="https://github.com/tjvig94/random-meal-generator"><i
                                         className="fab fa-github"></i> Code</Link>
                             </div>
                         </a>
@@ -50,13 +68,13 @@ function Portfolio() {
                     <div className="content">
                         <a href="https://lit-shore-77411.herokuapp.com/?id=607d95d51873600015c1794a" rel="noreferrer" target="_blank">
                             <div className="content-overlay"></div>
-                            <img className="content-image" src={FitnessTracker} alt="Dnd page" />
+                            <img className="content-image" src={FitnessTracker} alt="Fitness Tracker" />
                             <div className="content-details fadeIn-bottom">
                                 <h3 className="content-title">Fitness Tracker</h3>
                                 <p className="content-text">An app to help keep track of your workouts throughout the week. Click background to see page!</p>
                                 <Link
                                     style={{ textDecoration: "none", color: "grey" }}
-                                    className="content-text"
+                                    className="content-code-link"
                                     href="https://github.com/chadcourtney9/Fitness-Tracker"><i
                                         className="fab fa-github"></i> Code</Link>
                             </div>
@@ -67,13 +85,13 @@ function Portfolio() {
                     <div className="content">
                         <a href="https://chadcourtney9.github.io/Daily-Planner/" rel="noreferrer" target="_blank">
                             <div className="content-overlay"></div>
-                            <img className="content-image" src={RGen} alt="Dnd page" />
+                            <img className="content-image" src={RGen} alt="Random Number Generator" />
                             <div className="content-details fadeIn-bottom">
                                 <h3 className="content-title">Random Password Generator</h3>
                                 <p className="content-text">Generate a strong random password with selected characters! Click background to see page!</p>
                                 <Link
                                     style={{ textDecoration: "none", color: "grey" }}
-                                    className="content-text"
+                                    className="content-code-link"
                                     href="https://github.com/chadcourtney9/Random-Password-Gen"><i
                                         className="fab fa-github"></i> Code</Link>
                             </div>
@@ -84,37 +102,20 @@ function Portfolio() {
                     <div className="content">
                         <a href="https://stormy-taiga-31660.herokuapp.com/" rel="noreferrer" target="_blank">
                             <div className="content-overlay"></div>
-                            <img className="content-image" src={BudgetTracker} alt="Dnd page" />
+                            <img className="content-image" src={BudgetTracker} alt="Budget Tracker" />
                             <div className="content-details fadeIn-bottom">
                                 <h3 className="content-title">Budget Tracker</h3>
                                 <p className="content-text">This app helps you manage your spending online or offline! Click to see page!</p>
                                 <Link
                                     style={{ textDecoration: "none", color: "grey" }}
-                                    className="content-text"
+                                    className="content-code-link"
                                     href="https://github.com/chadcourtney9/budget-tracker"><i
                                         className="fab fa-github"></i> Code</Link>
                             </div>
                         </a>
                     </div>
                 </Container>
-                <Container className="portContainer col-md-6">
-                    <div className="content">
-                        <a href="https://character-creator-express.herokuapp.com/login" rel="noreferrer" target="_blank">
-                            <div className="content-overlay"></div>
-                            <img className="content-image" src={Planner} alt="Dnd page" />
-                            <div className="content-details fadeIn-bottom">
-                                <h3 className="content-title">Daily Planner</h3>
-                                <p className="content-text">Sort daily tasks based hourly to organize your day! Click background to see page!</p>
-                                <Link
-                                    style={{ textDecoration: "none", color: "grey" }}
-                                    className="content-text"
-                                    href="https://github.com/chadcourtney9/Daily-Planner"><i
-                                        className="fab fa-github"></i> Code</Link>
-                            </div>
-                        </a>
-                    </div>
 
-                </Container>
             </Row>
 
         </div >
