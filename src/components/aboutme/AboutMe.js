fcd6b850b142aaddd510d3024d9f8f9f9a5ab4fc
Chad Courtney <Chadcourtney567@gmail.com>
2021-08-05 16:16:45 -0400

    about me change

--- src/components/aboutme/AboutMe.js
+++ src/components/aboutme/AboutMe.js
@@ -14,7 +14,7 @@ class AboutMe extends Component {
                         <h1 className="amHeader">About Me</h1>
                         <img src={ChadCap} alt="lumberChad" className="lumberChad" />
                         <p className="amP">
-                            Hello, my name is Chad Courtney. I am a Full Stack Developer with a real strong eye for aesthetics. Also have strong teamwork and coordination skills after working with agile development practices creating full-stack web apps.  Always seeking to improve my skills, with both frontend and backend languages! If you're interested in working together, feel free to reach out to me through the any of the links below! Graduated from University of New Hampshire in Full Stack Web Development.
+                            Hello, my name is Chad Courtney. I am a full stack developer with a real strong eye for aesthetics. Also have strong teamwork and coordination skills after working with agile development practices creating full-stack web apps. Always seeking to improve my skills, with both frontend and backend languages! If you're interested in working together, feel free to reach out to me through the any of the links below! Graduated from University of New Hampshire in Full Stack Web Development.
                         </p>
                         <br></br>
                         <li><a style={{ textDecoration: "none", color: "grey" }} className="cool-link" href="tel: 9784573908" >Phone</a></li>
